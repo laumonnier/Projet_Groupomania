9d45348dd9e0e75dbe727030ddc7fced4ed54638
laumonnier <laurent.p.monnier86@gmail.com>
2022-09-15 11:34:52 +0200

    user.log file modified

--- controllers/user.log.js
+++ controllers/user.log.js
@@ -35,50 +35,6 @@ exports.login = (req, res, next) => {
             .catch(err => res.status(500).json({ error: err }));
                 
 };    
- 
-
-// exports.login = (req, res, next) => {
-        
-//     try {
-//         const user = User.login(email, password);
-//             const token = createToken(user._id)
-//             res.cookie('jwt', token, {httpOnly: true}, {maxAge: 12*60*60*1000})
-            // if(!user){
-            //     return res.status(401).json({ message: 'Incorrect login/password pair'});
-            // }
-            
-            // const userId = user._id
-            // const token = jwt.sign(
-            // { userId: userId}, 
-            // process.env.TOKEN,
-            // {expiresIn: '24h'})
-            // bcrypt.compare(req.body.password, user.password)
-                // .then(valid => {
-                //     if(!valid){
-                //         return res.status(401).json({ message: 'Incorrect login/password pair'});
-                //     }
-                    
-                    // res.status(200)
-                    
-                    // .json({
-                    //     userId: user._id,
-                    //     token: token
-                        // token : jwt.sign(
-                        //     { userId: user._id}, 
-                        //     process.env.TOKEN,
-                        //     {expiresIn: '24h'}
-            //         })
-            //     }catch (err){
-            //         res.status(401).json(err)
-            //     }
-            // }                
-                    
-                
-//                 .catch(err => res.status(500).json({ error: err }));      
-//         })    
-//         .catch(err => res.status(500).json({ error: err }))        
-// });     
-// }
 
 exports.logout = (req, res, next) => {
     // res.status(200).clearCookie('jwt', {path:'/'});
