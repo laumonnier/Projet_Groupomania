a300e7d7a616c0c529bd9d7667bebfc6485fc116
laumonnier <laurent.p.monnier86@gmail.com>
2022-09-21 20:26:34 +0200

    Start of multer to be able to put photos within the social site

--- controllers/upload.control.js
+++ controllers/upload.control.js
@@ -4,22 +4,43 @@ const { promisify } = require('util');
 const { uploadErrors } = require('../utils/errors');
 const pipeline = promisify(require('stream').pipeline);
 
-exports.uploadUserProfil = (req, res, next) => {
-    try{
-        if(
-           req.file.detectedMimeType !== "image/jpg" &&
-           req.file.detectedMimeType !== "image/jpeg" &&
-           req.file.detectedMimeType !== "image/png" 
-        )
-        throw Error("invalid file");
+exports.uploadUserProfil = (req, res, next) => { //Gestion de multer autrement concernant les images du personnel
+    // A voir
+    const MIME_TYPES = {
+        'image/jpg': 'jpg',
+        'image/jpeg': 'jpg',
+        'image/png': 'png'
+    };
 
-        if(req.file.size > 500000)
-        throw Error("max size");
-    }
-    catch (err){
-        const errors = uploadErrors(err)
-        res.status(401).json({ errors });
-    }
+    const storage = multer.diskStorage({
+        destination: (res, file, callback) => {
+            callback(null, 'images');
+        },
+        filename: (req, file, callback) => {
+            const name = file.originalname.split(' ').join('_');
+            const extension = MIME_TYPES[file.mimetype];
+            callback(null, name + Date.now() + '.' + extension);
+        }
+    });
+
+    multer({storage: storage}).single('image');
+
+
+    // try{
+    //     if(
+    //        req.file.detectedMimeType !== "image/jpg" &&
+    //        req.file.detectedMimeType !== "image/jpeg" &&
+    //        req.file.detectedMimeType !== "image/png" 
+    //     )
+    //     throw Error("invalid file");
+
+    //     if(req.file.size > 500000)
+    //     throw Error("max size");
+    // }
+    // catch (err){
+    //     const errors = uploadErrors(err)
+    //     res.status(401).json({ errors });
+    // }
 
     const fileName = req.body.name + ".jpg";
 
